3847eba459fbab33022053b9d40b7a84893d94b4
Chiara <chiarapanato22@gmail.com>
2022-06-17 22:09:19 +0200

    creted detail page

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,6 +7,36 @@ import { exerciseOptions, fetchData } from '../utils/fetchData'
 
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+
+  const [ currentPage, setCurrentPage ]= useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+
+  const paginate= (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior:'smooth' })
+  }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exerciseData = [];
+
+      if(bodyPart === 'all') {
+        exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exerciseData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exerciseData);
+    }
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
   return (
     <Box id="exercises"
     sx={{ mt: { lg: '110px' }}}
@@ -20,9 +50,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       flexWrap="wrap"
       justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
            <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+          {exercises.length > 9 && (
+            <Pagination 
+            color="standard" 
+            shape="rounded" 
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage )} 
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+            />
+          )}
       </Stack>
     </Box>
   )
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+import { ExerciseOption, exerciseOptions, fetchData } from '../utils/fetchData';
 
 const ExerciseDetails = () => {
+
+  const [ exerciseDetail, setexerciseDetail ] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSesrchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions)
+      setexerciseDetail(exerciseDetailData);
+    }
+
+    fetchExercisesData();
+  }, [id]);
+
   return (
-    <div>ExerciseDetails</div>
+    <Box>
+      <Detail exerciseDetail= {exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
-export default ExerciseDetails
+export default ExerciseDetails;
